a1f58d03a9a25097684d2bb1bf8503c2e7f68b8e
Jintu-sudo <jintubasy@outlook.com>
2024-11-24 13:08:02 +0530

    A few teaks

--- assigment2/app.js
+++ assigment2/app.js
@@ -1,22 +1,26 @@
-// app.js
-var app = angular.module('myApp', []);
+var app = angular.module('personalApp', ['ngRoute']);
 
-app.controller('myController', function($scope) {
-    $scope.name = "Your Name";
-    $scope.tagline = "A brief tagline about you";
-    $scope.about = "This is a brief description about yourself.";
-    
-    $scope.hobbies = ["Reading", "Traveling", "Cooking", "Photography"];
-    $scope.village = "Your Native Village/Town";
-    
-    $scope.family = ["Father: Name", "Mother: Name", "Sibling: Name"];
-    
-    $scope.schooling = "Details about your schooling.";
-    $scope.collegeLife = "Details about your college experience.";
-    
-    $scope.qualifications = ["High School Diploma", "Bachelor's Degree in Subject"];
-    
-    $scope.rewards = ["Award 1", "Award 2", "Certificate of Excellence"];
-    
-    $scope.contributions = "Details about your contributions to society.";
+app.config(function($routeProvider) {
+  $routeProvider
+    .when('/home', {
+      templateUrl: 'components/home.html'
+    })
+    .when('/about', {
+      templateUrl: 'components/about.html'
+    })
+    .when('/education', {
+      templateUrl: 'components/education.html'
+    })
+    .when('/family', {
+      templateUrl: 'components/family.html'
+    })
+    .when('/hobbies', {
+      templateUrl: 'components/hobbies.html'
+    })
+    .when('/contributions', {
+      templateUrl: 'components/contributions.html'
+    })
+    .otherwise({
+      redirectTo: '/home'
+    });
 });
